0880a615e989257cbbb6cf5880ee43613b5fb3cf
jakubpoltoraczyk <jakubpoltoraczyk@onet.pl>
2020-04-10 19:27:13 +0200

    Zmiana na size = 4

--- const_value.h
+++ const_value.h
@@ -2,7 +2,7 @@
 #define CONST_VALUE_H
 
 /* Plik z definicjami stałych potrzebnych w programie */
-const int size=3;
+const int size=4;
 const double epsilon=0.0000000001;
 
 #endif // CONST_VALUE_H
